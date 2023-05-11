93f28c1605d5b959e2f4806609b983f2e7a2a277
Kuyetdev.forwork <kuyetdev.forwork@gmail.com>
2023-05-11 17:28:58 +0700

    first commit

---
+++ Code/Exercise2/Assets/Script/PrintFavoriteGames.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintFavoriteGames : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Debug.Log("Valorant");
+        UnityEngine.Debug.Log("Fifa");
+        UnityEngine.Debug.Log("Dota");
+    }
+
+}
